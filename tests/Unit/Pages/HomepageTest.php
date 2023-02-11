e335b31fc71e64a8a59aa96c6a0927306a2f097c
Bruno Görß <39590058+Katalam@users.noreply.github.com>
2023-02-11 21:53:25 +0100

    Adding homepage tests

---
+++ tests/Unit/Pages/HomepageTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+use Inertia\Testing\AssertableInertia as Assert;
+
+test('it shows the homepage', function () {
+    $user = User::factory()->create();
+    $posts = Post::factory(100)->create();
+    foreach ($posts as $post) {
+        $post->likes()->create([
+            'user_id' => $user->id,
+        ]);
+    }
+
+    $response = $this->actingAs($user)
+        ->get(route('home'));
+
+    $response->assertStatus(200);
+    $response->assertInertia(fn(Assert $page) => $page
+        ->component('Homepage')
+        ->has('posts', 20, fn(Assert $posts) => $posts
+            ->has('id')
+            ->has('body')
+            ->has('created_at')
+            ->has('updated_at')
+            ->has('liked_by_current_user')
+            ->has('likes_count')
+            ->has('user_id')
+            ->has('image')
+            ->has('created_at_human')
+            ->has('deleted_at')
+            ->has('likes.0', fn(Assert $likes) => $likes
+                ->has('id')
+                ->has('user_id')
+                ->has('post_id')
+                ->has('created_at')
+                ->has('updated_at')
+            )
+            ->has('user', fn(Assert $user) => $user
+                ->has('id')
+                ->has('name')
+            )
+        )
+        ->has('users', 3, fn(Assert $users) => $users
+            ->has('id')
+            ->has('name')
+            ->has('username')
+        )
+    );
+})->group('group', 'homepage');
